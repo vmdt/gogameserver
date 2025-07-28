775b8da665e7694c13a27099ac6cde8bf977c76a
datvobigin <dat.vo@bigin.vn>
2025-07-28 16:48:17 +0700

    user domain methods

--- modules/identity/domain/user.go
+++ modules/identity/domain/user.go
@@ -1,6 +1,11 @@
 package domain
 
-import "time"
+import (
+	"time"
+
+	"github.com/vmdt/gogameserver/pkg/cryptography/hasher"
+	"gorm.io/gorm"
+)
 
 type User struct {
 	ID           string `gorm:"type:uuid;primaryKey" json:"id"`
@@ -16,4 +21,34 @@ type User struct {
 
 func (u *User) TableName() string {
 	return "identity_users"
+}
+
+func (u *User) BeforeCreate(db *gorm.DB) (err error) {
+	if u.PasswordHash == "" {
+		u.GenPassv2()
+	}
+	return
+}
+
+func (u *User) BeforeUpdate(db *gorm.DB) (err error) {
+	if u.Password != "" {
+		u.GenPassv2()
+	}
+	return
+}
+
+func (u *User) GenPassv2() {
+	u.PasswordHash, _ = hasher.GenerateFromPassword(u.Password)
+}
+
+func (u *User) ValidatePassword(password string) (bool, error) {
+	return hasher.ComparePasswordAndHash(password, u.PasswordHash)
+}
+
+func (u *User) ChangePassword(password string, newPassword string) error {
+	result, err := hasher.ComparePasswordAndHash(password, u.PasswordHash)
+	if err == nil && result {
+		u.Password = newPassword
+	}
+	return err
 }
